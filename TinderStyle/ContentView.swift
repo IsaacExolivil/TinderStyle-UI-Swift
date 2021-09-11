4723ced339d4c7baa5007df3568a1988820f08b9
Strider <exolivil@gmail.com>
2021-09-11 01:29:38 -0500

    Initial Commit

---
+++ TinderStyle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinderStyle
+//
+//  Created by Isaac Loez on 11/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinderStyle/TinderStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderStyleApp.swift
+//  TinderStyle
+//
+//  Created by Isaac Loez on 11/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TinderStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
